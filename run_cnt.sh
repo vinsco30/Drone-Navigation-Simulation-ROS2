d403483dcea2a6d21498b74bba79e3698242f3ea
Vincenzo Scognamiglio <vincenzo.scognamiglio2@unina.it>
2025-07-04 17:36:44 +0200

    setup for headless simulation with depth camera

--- run_cnt.sh
+++ run_cnt.sh
@@ -10,8 +10,10 @@ docker run --rm -it --privileged \
 -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 -v "/dev:/dev" \
 -v $(pwd)/PX4-Autopilot-PRISMA:/root/PX4-Autopilot-PRISMA:rw \
+-v $(pwd)/PX4-Autopilot:/root/PX4-Autopilot:rw \
 -v $(pwd)/ros2_ws-src/pkg:/root/ros2_ws/src:rw \
 -v $(pwd)/start:/root/start:rw \
+-v $(pwd)/planner_4_octo:/root/planner_4_octo:rw \
 --env="DISPLAY=$DISPLAY" \
 --network host \
 --name=px4-ros2-gz px4-ros2-gz bash
